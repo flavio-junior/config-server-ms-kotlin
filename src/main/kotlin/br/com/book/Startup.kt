be7f1ba133cf271937b6450f64bcc1ba04381835
flavio-junior <flaviojunior.work@gmail.com>
2023-11-23 16:14:31 -0300

    Zookeeper implementing in project

--- src/main/kotlin/br/com/book/Startup.kt
+++ src/main/kotlin/br/com/book/Startup.kt
@@ -2,9 +2,11 @@ package br.com.book
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 class Startup
 
