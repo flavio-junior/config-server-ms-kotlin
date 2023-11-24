4d72d2b4d560b7823c0dd8cc6eac54635383cd45
flavio-junior <flaviojunior.work@gmail.com>
2023-11-24 09:02:26 -0300

    project created with configs to a aapi gateway

---
+++ src/main/kotlin/br/com/filter/LoggerFilter.kt
@@ -0,0 +1,19 @@
+package br.com.filter
+
+import org.slf4j.LoggerFactory
+import org.springframework.cloud.gateway.filter.GatewayFilterChain
+import org.springframework.cloud.gateway.filter.GlobalFilter
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+class LoggerFilter: GlobalFilter {
+
+    private val logger = LoggerFactory.getLogger(LoggerFilter::class.java)
+
+    override fun filter(exchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> {
+        logger.info("Original request path -> {}", exchange.request.path)
+        return chain.filter(exchange)
+    }
+}
